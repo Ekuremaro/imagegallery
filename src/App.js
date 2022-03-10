de2029a02d2e3c10369761ea8004bf218c35e2ce
Ekuremaro <86929331+Ekuremaro@users.noreply.github.com>
2022-03-10 02:02:03 +0100

    Created app with create react app, configured tailwindcss

---
+++ src/App.js
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img
+        src="https://source.unsplash.com/random"
+        alt="image1"
+        className=" w-full"
+      />
+
+      <div className="px-6 py-4">
+        <div className=" font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            4000
+          </li>
+          <li>
+            <strong>Views: </strong>
+            4000
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default App;
